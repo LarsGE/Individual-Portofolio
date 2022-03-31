fa1b994b438b7969a77042cf18232d6388716c5c
LarsGE <70587035+LarsGE@users.noreply.github.com>
2022-03-31 22:47:29 +0200

    add inline comments

--- client.py
+++ client.py
@@ -1,14 +1,13 @@
 import socket
 import random
 import argparse
-import time
 import threading
-import sys
 
-
+# Format and disconnect message
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "DC"
 
+# Parser that makes sure the necessary arguments are provided
 argParser = argparse.ArgumentParser(description='Connect to chat room.')
 argParser.add_argument('IP', type=str, help='IP address of the server the client is connecting to.')
 argParser.add_argument('port', type=int, help='The port the client is connecting to.')
@@ -18,88 +17,93 @@ IP = args.IP
 port = args.port
 name = args.name
 
+# TCP socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((IP, port))
 
-
+# Bot names
 bots = ["ola", "seb", "fred"]
 
+# List of good and bat actions
+good_actions = ["laugh", "play", "relax", "read", "train", "sleep"]
+bad_actions = ["scream", "fight", "moan", "bicker", "complain", "kill"]
 
-good_things = ["laugh", "play", "relax", "read", "train", "sleep"]
-bad_things = ["scream", "fight", "moan", "bicker", "complain", "kill"]
 
-
-# all bot functions
+# Bot functions
 def ola(verb):
-    if verb in good_things:
-        altAnswers = [
-        "{}: WOW!{} sounds amazing!".format(name,verb + "ing"),
-        "{}: I think {} is fantastic!".format(name,verb + "ing"),
-        "{}: Let's go {}!".format(name,verb + "ing")
-    ]
-        return random.choice(altAnswers)
+    if verb in good_actions:
+        altanswers = [
+            "{}: WOW!{} sounds amazing!".format(name,verb + "ing"),
+            "{}: I think {} is fantastic!".format(name,verb + "ing"),
+            "{}: Let's go {}!".format(name,verb + "ing")
+        ]
+        return random.choice(altanswers)
 
-    elif verb in bad_things:
-        altAnswers2 = [
-        "{}: {}?! Are you insane?!".format(name,verb + "ing"),
-        "{}: I'm not really into {}, sorry.".format(name,verb + "ing"),
-        "{}: I'm can not agree to {}".format(name,verb+"ing")
-    ]
-        return random.choice(altAnswers2)
+    elif verb in bad_actions:
+        altanswers2 = [
+            "{}: {}?! Are you insane?!".format(name,verb + "ing"),
+            "{}: I'm not really into {}, sorry.".format(name,verb + "ing"),
+            "{}: I'm can not agree to {}".format(name,verb+"ing")
+        ]
+        return random.choice(altanswers2)
 
     else:
         return "{}: Sorry, can you repeat that".format(name)
 
+
 def seb(verb):
-    if verb in good_things:
-        altAnswers3 = [
+    if verb in good_actions:
+        altanswers3 = [
             "{}: {}? Can't complain".format(name,verb + "ing"),
             "{}: {}, can't wait to start".format(name,verb + "ing"),
             "{}: I actually enjoy {}".format(name,verb+"ing")
         ]
-        return random.choice(altAnswers3)
+        return random.choice(altanswers3)
 
-    elif verb in bad_things:
-        altAnswers4 = [
+    elif verb in bad_actions:
+        altanswers4 = [
             "{}: NO!".format(name),
             "{}: {}, is not for me.".format(name,verb + "ing"),
             "{}: I think I'll pass on {}".format(name,verb + "ing")
         ]
-        return random.choice(altAnswers4)
+        return random.choice(altanswers4)
+
 
 def fred(verb):
-    if verb in good_things:
-        altAnswers5 = [
-        "{}: {}?? I am actually excited about that.".format(name,verb+"ing"),
-        "{}: You know i like {}!".format(name,verb + "ing"),
-        "{}: {}? I mean it's allright".format(name,verb+"ing")
-    ]
-        return random.choice(altAnswers5)
+    if verb in good_actions:
+        altanswers5 = [
+            "{}: {}?? I am actually excited about that.".format(name,verb+"ing"),
+            "{}: You know i like {}!".format(name,verb + "ing"),
+            "{}: {}? I mean it's allright".format(name,verb+"ing")
+        ]
+        return random.choice(altanswers5)
 
-    elif verb in bad_things:
-        altAnswers6 = [
-        "{}: {}? I don't like that".format(name,verb+"ing"),
-        "{}: {}, if everyone else want's to, im in.".format(name,verb + "ing"),
-        "{}: I mean.{} isn't horrible.".format(name,verb+"ing")
-    ]
-        return random.choice(altAnswers6)
+    elif verb in bad_actions:
+        altanswers6 = [
+            "{}: {}? I don't like that".format(name,verb+"ing"),
+            "{}: {}, if everyone else want's to, im in.".format(name,verb + "ing"),
+            "{}: I mean.{} isn't horrible.".format(name,verb+"ing")
+        ]
+        return random.choice(altanswers6)
 
     else:
         return "{}: Can you rephrase that".format(name)
 
 
+# Function that receives the message from the clients
 def receivemessage():
     while True:
         message = client.recv(2048).decode(FORMAT)
         print(message)
 
 
+# Function that sends the message to the clients
 def clientsend(msg):
-    # print(msg)
     message = msg.encode(FORMAT)
     client.send(message)
 
 
+# Function that takes clients input
 def messages():
     msgs = True
     while msgs:
@@ -113,7 +117,7 @@ def messages():
             print("\nPlease type something!")
             continue
 
-
+# Threads for the messages and receive functions
 message_thread = threading.Thread(target=messages)
 message_thread.start()
 receive_thread = threading.Thread(target=receivemessage)
--- server.py
+++ server.py
@@ -1,34 +1,38 @@
-
 import socket
 import threading
 import argparse
 
+# Format and a disconnect message
 FORMAT = 'utf-8'
 DISCONNECT_MSG = "DC"
 
+# Parser to make sure the port is part of the argument
 argParser = argparse.ArgumentParser(description='Start the chat server and listen for incoming connections.')
 argParser.add_argument('port', type=int, help='The port the server is running on.')
 args = argParser.parse_args()
 port = args.port
 
+# TCP socket and the clients and their names
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('localhost', port))
 server.listen()
 clients = []
 clientNames = []
 
-
+# A function that sends message to all clients
 def broadcast(message, conn):
     for i in clients:
         if i is not conn:
             i.send(message)
 
-
+# A function that handles new clients
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
 
     while True:
         msg = conn.recv(1024).decode(FORMAT)
+        if (msg == "SHUTDOWN"):
+            print("Disconnecting clients")
         broadcast(msg.encode(FORMAT), conn)
         if msg == DISCONNECT_MSG:
             broadcast(f'\nUser is now disconnected from chat room\n'.encode(FORMAT), conn)
@@ -38,7 +42,7 @@ def handle_client(conn, addr):
         print(f"[{addr}] {msg}")
     conn.close()
 
-
+# The function that starts the server
 def start():
     server.listen()
     print(f"[LISTENING] Server is listening...")
